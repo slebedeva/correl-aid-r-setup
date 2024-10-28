df5d39195258df7894288359bea2d858964dbefa
Svetlana Lebedeva <slebedeva@gmail.com>
2024-10-28 12:29:29 +0100

    saved rstudio settings file and mount it on the container to have the settings different from default load automatically

--- run_docker_local_mount.sh
+++ run_docker_local_mount.sh
@@ -6,6 +6,8 @@ if [ $# -ne 1 ]; then
 	echo "Using default value, the parent directory .."
 fi
 
+echo "using port:"
+echo "localhost:8787"
 
 # Path to the parent of the cloned CorrelAid github repository
 #CORREL_HOME=$1
@@ -19,12 +21,17 @@ mkdir -p $RENV_PATHS_CACHE_HOST
 RENV_PATHS_CACHE_CONTAINER=/home/rstudio/.cache/R/renv
 
 # run the container with the host cache mounted in the container
+# also mount Rstudio settings file ~/.config/rstudio/rstudio-prefs.json
 docker run --rm \
     -e "RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER}" \
     -v "${RENV_PATHS_CACHE_HOST}:${RENV_PATHS_CACHE_CONTAINER}" \
     -v "${CORREL_HOME}:/home/rstudio/CorrelAid"  \
+    -v "${PWD}/rstudio-settings:/home/rstudio/.config/rstudio" \
     -e "PASSWORD=foobar"  \
     -p 8787:8787 \
     correl-aid:latest #replace with your container name
+
+# start rstudio in a browser
+xdg-open http://localhost:8787
 
 exit 0
